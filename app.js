d727e151803424670220af843ee272a5f0c275e0
Antkeo <40493888+Antkeo@users.noreply.github.com>
2018-11-04 13:09:28 -0500

    my commit

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example')
+
+// use require without a reference to ensure a file is bundled
+// require('./example')
+const authEvents = require('./event.js')
+
+$(() => {
+  // your JS code goes here
+  $('#signIn').on('submit', authEvents.onSignUp)
+})
